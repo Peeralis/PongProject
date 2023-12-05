4c6ccc92626e97a0164ccbbb9e60d658b95dbf13
Peem <mrptvi@gmail.com>
2023-12-05 22:28:07 +0700

    finish before test vga

--- PongProject.srcs/sources_1/new/left_counter.v
+++ PongProject.srcs/sources_1/new/left_counter.v
@@ -21,6 +21,53 @@
 
 
 module left_counter(
-
+    input clk,
+    input reset,
+    input d_incl, d_clr,
+    output [3:0] digl0, digl1
     );
+    
+    // signal declaration
+    reg [3:0] r_digl0, r_digl1, digl0_next, digl1_next;
+    
+    // register control
+    always @(posedge clk or posedge reset)
+        if(reset) begin
+            r_digl1 <= 0;
+            r_digl0 <= 0;
+        end
+        
+        else begin
+            r_digl1 <= digl1_next;
+            r_digl0 <= digl0_next;
+        end
+    
+    // next state logic
+    always @* begin
+        digl0_next = r_digl0;
+        digl1_next = r_digl1;
+        
+        if(d_clr) begin
+            digl0_next <= 0;
+            digl1_next <= 0;
+        end
+        
+        else if(d_incl)
+            if(r_digl0 == 9) begin
+                digl0_next = 0;
+                
+                if(r_digl1 == 9)
+                    digl1_next = 0;
+                else
+                    digl1_next = r_digl1 + 1;
+            end
+        
+            else    // dig0 != 9
+                digl0_next = r_digl0 + 1;
+    end
+    
+    // output
+    assign digl0 = r_digl0;
+    assign digl1 = r_digl1;
+    
 endmodule
--- PongProject.srcs/sources_1/new/right_counter.v
+++ PongProject.srcs/sources_1/new/right_counter.v
@@ -21,6 +21,53 @@
 
 
 module right_counter(
-
+    input clk,
+    input reset,
+    input d_incr, d_clr,
+    output [3:0] digr0, digr1
     );
+    
+    // signal declaration
+    reg [3:0] r_digr0, r_digr1, digr0_next, digr1_next;
+    
+    // register control
+    always @(posedge clk or posedge reset)
+        if(reset) begin
+            r_digr1 <= 0;
+            r_digr0 <= 0;
+        end
+        
+        else begin
+            r_digr1 <= digr1_next;
+            r_digr0 <= digr0_next;
+        end
+    
+    // next state logic
+    always @* begin
+        digr0_next = r_digr0;
+        digr1_next = r_digr1;
+        
+        if(d_clr) begin
+            digr0_next <= 0;
+            digr1_next <= 0;
+        end
+        
+        else if(d_incr)
+            if(r_digr0 == 9) begin
+                digr0_next = 0;
+                
+                if(r_digr1 == 9)
+                    digr1_next = 0;
+                else
+                    digr1_next = r_digr1 + 1;
+            end
+        
+            else    // dig0 != 9
+                digr0_next = r_digr0 + 1;
+    end
+    
+    // output
+    assign digr0 = r_digr0;
+    assign digr1 = r_digr1;
+    
 endmodule
